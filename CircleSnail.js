fe835541c4fe799a86aa2f039e34c7bcfdb03257
Farida Mohammed <fareda.abuelfutuh@gmail.com>
2022-01-20 15:05:14 +0200

    add circle import

--- CircleSnail.js
+++ CircleSnail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Animated, Easing } from 'react-native';
-import { Svg } from 'react-native-svg';
+import { Svg, Circle } from 'react-native-svg';
 
 import Arc from './Shapes/Arc';
 
